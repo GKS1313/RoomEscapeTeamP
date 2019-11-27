cf892e5721d161947a5bdce840061e4dea5b8c7d
GKS1313 <qowlgks@gmail.com>
2019-11-27 18:14:20 +0900

    임시 소스

---
+++ 소스파일 - 복사본.js
@@ -0,0 +1,459 @@
+﻿Function.prototype.member = function(name, value){
+	this.prototype[name] = value
+}
+
+//////// Game Definition
+function Game(){}
+Game.start = function(room, welcome){
+	game.start(room.id)
+	printMessage(welcome)
+}
+Game.end = function(){
+	game.clear()
+}
+Game.move = function(room){
+	game.move(room.id)	
+}
+Game.handItem = function(){
+	return game.getHandItem()
+}
+
+
+//////// Room Definition
+
+function Room(name, background){
+	this.name = name
+	this.background = background
+	this.id = game.createRoom(name, background)
+}
+Room.member('setRoomLight', function(intensity){
+	this.id.setRoomLight(intensity)
+})
+
+//////// Object Definition
+
+function Object(room, name, image){
+	this.room = room
+	this.name = name
+	this.image = image
+
+	if (room !== undefined){
+		this.id = room.id.createObject(name, image)
+	}
+}
+Object.STATUS = { OPENED: 0, CLOSED: 1, LOCKED: 2 }
+
+Object.member('setSprite', function(image){
+	this.image = image
+	this.id.setSprite(image)
+})
+Object.member('resize', function(width){
+	this.id.setWidth(width)
+})
+Object.member('setDescription', function(description){
+	this.id.setItemDescription(description)
+})
+
+Object.member('getX', function(){
+	return this.id.getX()
+})
+Object.member('getY', function(){
+	return this.id.getY()
+})
+Object.member('locate', function(x, y){
+	this.room.id.locateObject(this.id, x, y)
+})
+Object.member('move', function(x, y){
+	this.id.moveX(x)
+	this.id.moveY(y)
+})
+
+Object.member('show', function(){
+	this.id.show()
+})
+Object.member('hide', function(){
+	this.id.hide()
+})
+Object.member('open', function(){
+	this.id.open()
+})
+Object.member('close', function(){
+	this.id.close()
+})
+Object.member('lock', function(){
+	this.id.lock()
+})
+Object.member('unlock', function(){
+	this.id.unlock()
+})
+Object.member('isOpened', function(){
+	return this.id.isOpened()
+})
+Object.member('isClosed', function(){
+	return this.id.isClosed()
+})
+Object.member('isLocked', function(){
+	return this.id.isLocked()
+})
+Object.member('pick', function(){
+	this.id.pick()
+})
+Object.member('isPicked', function(){
+	return this.id.isPicked()
+})
+
+//////// Door Definition
+
+function Door(room, name, closedImage, openedImage, connectedTo){
+	Object.call(this, room, name, closedImage)
+
+	// Door properties
+	this.closedImage = closedImage
+	this.openedImage = openedImage
+	this.connectedTo = connectedTo
+}
+// inherited from Object
+Door.prototype = new Object()
+
+Door.member('onClick', function(){
+	if (!this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	}
+	else if (this.id.isOpened()){
+		if (this.connectedTo !== undefined){
+			Game.move(this.connectedTo)
+		}
+		else {
+			Game.end()
+		}
+	}
+})
+Door.member('onOpen', function(){
+	this.id.setSprite(this.openedImage)
+})
+Door.member('onClose', function(){
+	this.id.setSprite(this.closedImage)
+})
+
+
+//////// Keypad Definition
+
+function Keypad(room, name, image, password, callback){
+	Object.call(this, room, name, image)
+
+	// Keypad properties
+	this.password = password
+	this.callback = callback
+}
+// inherited from Object
+Keypad.prototype = new Object()
+
+Keypad.member('onClick', function(){
+	showKeypad('number', this.password, this.callback)
+})
+
+
+//////// DoorLock Definition
+function DoorLock(room, name, image, password, door, message){
+	Keypad.call(this, room, name, image, password, function(){
+		printMessage(message)
+		door.unlock()
+	})
+}
+// inherited from Object
+DoorLock.prototype = new Keypad()
+
+/////// Item Definition
+
+function Item(room, name, image){
+	Object.call(this, room, name, image)
+}
+// inherited from Object
+Item.prototype = new Object()
+
+Item.member('onClick', function(){
+	this.id.pick()
+})
+
+
+Item.member('isHanded', function(){
+	return Game.handItem() == this.id
+})
+
+//이까지 재정의
+
+
+
+
+room1 = new Room('room1', '배경-1.png')		// 변수명과 이름이 일치해야 한다.
+room2 = new Room('room2', '배경-2.png')		// 변수명과 이름이 일치해야 한다.
+room3 = new Room('room3', '배경-3.png')		// 변수명과 이름이 일치해야 한다.
+
+room1.door1 = new Door(room1, 'door1', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room2)
+room1.door1.resize(120)
+room1.door1.locate(1000, 312)
+room1.door1.lock()
+
+room1.key1 = new Item(room1, 'key1', '솜.png')
+room1.key1.resize(45)
+room1.key1.locate(745, 500)
+
+
+
+//Item.member('onClick', function(){
+//	this.id.pick()
+//})
+
+Item.member('onClick', function(){printMessage(this.name)})
+
+
+
+goldfishcounter=0
+room1.goldfish=new Item(room1, 'goldfish', '금붕어.png')
+room1.goldfish.resize(200)
+room1.goldfish.locate(100, 200)
+room1.goldfish.onClick=function(){
+	if(goldfishcounter==0)
+	{
+		printMessage("금붕어 설명")
+		goldfishcounter++
+	}
+	else
+	{
+		room1.goldfish.pick()
+		printMessage("금붕어 획득")
+	}
+
+}
+
+milkpowdercounter=0
+room1.milkpowder=new Item(room1, 'milkpowder', '애기.png')
+room1.milkpowder.resize(200)
+room1.milkpowder.locate(300, 500)
+room1.milkpowder.onClick=function(){
+	if(milkpowdercounter==0)
+	{
+		printMessage("분유통 설명")
+		milkpowdercounter++
+	}
+	else
+	{
+		room1.milkpowder.pick()
+		printMessage("분유통 획득")
+	}
+
+}
+
+sojucounter=0
+room1.soju=new Item(room1, 'soju', '소주병.png')
+room1.soju.resize(200)
+room1.soju.locate(400, 300)
+room1.soju.onClick=function(){
+	if(sojucounter==0)
+	{
+		printMessage("소주 설명")
+		sojucounter++
+	}
+	else
+	{
+		room1.soju.pick()
+		printMessage("소주 획득")
+	}
+
+}
+
+envelopecounter=0
+room1.envelope=new Item(room1, 'envelope', '돈.png')
+room1.envelope.resize(200)
+room1.envelope.locate(500, 200)
+room1.envelope.onClick=function(){
+	if(envelopecounter==0)
+	{
+		printMessage("돈봉투 설명")
+		envelopecounter++
+	}
+	else
+	{
+		room1.envelope.pick()
+		printMessage("돈봉투 획득")
+	}
+
+}
+
+Item.member('onClick', function(Item){
+	printMessage(Item.name)
+})
+
+//고정 객체
+room1.cradle=new Item(room1, 'cradle', '요람.png')
+room1.cradle.resize(200)
+room1.cradle.locate(400, 200)
+room1.cradle.close()
+room1.cradle.onClick(room1.cradle)
+
+
+
+
+room1.carpet=new Item(room1, 'carpet', '카페트.png')
+room1.carpet.resize(200)
+room1.carpet.locate(200, 300)
+room1.carpet.close()
+room1.carpet.onClick=function(){
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.envelope.isHanded())
+			{
+				this.id.open()
+				printMessage("envelope")
+		
+			}
+			else
+			{
+				printMessage("잘못 넣음 게임 종료")
+		
+			}
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+room1.sofa=new Item(room1, 'sofa', 't1.png')
+room1.sofa.resize(50)
+room1.sofa.locate(300, 300)
+room1.sofa.close()
+room1.sofa.onClick=function(){
+
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.soju.isHanded())
+			{
+				this.id.open()
+				printMessage("soju")
+		
+			}
+			else
+			{
+				printMessage("잘못 넣음 게임 종료")
+		
+			}
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+room1.fishbowl=new Item(room1, 'fishbowl', '어항.png')
+room1.fishbowl.resize(200)
+room1.fishbowl.locate(200, 200)
+room1.fishbowl.close()
+room1.fishbowl.onClick=function(){
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.fishbowl.isHanded())
+			{
+				this.id.open()
+				printMessage("goldfish")
+		
+			}
+			else
+			{
+				printMessage("잘못 넣음 게임 종료")
+		
+			}
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+room1.door1.onClick = function(){
+
+	if(room1.door1.isOpened()){
+		Game.move(room2)
+	}
+	else if(room1.cradle.isOpened()&&room1.fishbowl.isOpened()&&room1.carpet.isOpened()&&room1.sofa.isOpened())
+	{
+		room1.door1.open()	
+	}
+	else if(room1.door1.isLocked())
+	{
+		printMessage("잠겨있음")		
+	}
+	
+	
+
+//	if (room1.key1.isHanded() && !this.id.isLocked() && this.id.isClosed()){
+//		this.id.open()
+//	}
+//	else if (this.id.isOpened()){
+//		if (this.connectedTo !== undefined){
+//			Game.move(this.connectedTo)
+//		}
+//		else {
+//			Game.end()
+//		}
+//	}
+}
+
+
+room2.door1 = new Door(room2, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room1)
+room2.door1.resize(120)
+room2.door1.locate(300, 270)
+
+room2.door2 = new Door(room2, 'door2', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room3)
+room2.door2.resize(120)
+room2.door2.locate(1000, 305)
+room2.door2.hide()
+
+room2.keypad1 = new Keypad(room2, 'keypad1', '숫자키-우.png', '1234', function(){
+	printMessage('스르륵 문이 보인다')
+	room2.door2.show()
+})
+room2.keypad1.resize(20)
+room2.keypad1.locate(920, 250)
+
+// onClick 함수를 재정의할 수도 있다
+room2.keypad1.onClick = function(){
+	printMessage('1234')
+	showKeypad('number', this.password, this.callback)
+}
+
+room3.door1 = new Door(room3, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room2)
+room3.door1.resize(120)
+room3.door1.locate(300, 297)
+
+room3.door2 = new Door(room3, 'door2', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png')
+room3.door2.resize(120)
+room3.door2.locate(1000, 313)
+room3.door2.lock()
+
+room3.lock1 = new DoorLock(room3, 'lock1', '숫자키-우.png', '1234', room3.door2, '철커덕')
+room3.lock1.resize(20)
+room3.lock1.locate(920, 250)
+
+Game.start(room1, '방탈출에 오신 것을 환영합니다!')
---
+++ 소스파일.js
@@ -0,0 +1,726 @@
+﻿Function.prototype.member = function(name, value){
+	this.prototype[name] = value
+}
+
+//////// Game Definition
+function Game(){}
+Game.start = function(room, welcome){
+	game.start(room.id)
+	printMessage(welcome)
+}
+Game.end = function(){
+	game.clear()
+}
+Game.move = function(room){
+	game.move(room.id)	
+}
+Game.handItem = function(){
+	return game.getHandItem()
+}
+
+
+//////// Room Definition
+
+function Room(name, background){
+	this.name = name
+	this.background = background
+	this.id = game.createRoom(name, background)
+}
+Room.member('setRoomLight', function(intensity){
+	this.id.setRoomLight(intensity)
+})
+
+//////// Object Definition
+
+function Object(room, name, image){
+	this.room = room
+	this.name = name
+	this.image = image
+
+	if (room !== undefined){
+		this.id = room.id.createObject(name, image)
+	}
+}
+Object.STATUS = { OPENED: 0, CLOSED: 1, LOCKED: 2 }
+
+Object.member('setSprite', function(image){
+	this.image = image
+	this.id.setSprite(image)
+})
+Object.member('resize', function(width){
+	this.id.setWidth(width)
+})
+Object.member('setDescription', function(description){
+	this.id.setItemDescription(description)
+})
+
+Object.member('getX', function(){
+	return this.id.getX()
+})
+Object.member('getY', function(){
+	return this.id.getY()
+})
+Object.member('locate', function(x, y){
+	this.room.id.locateObject(this.id, x, y)
+})
+Object.member('move', function(x, y){
+	this.id.moveX(x)
+	this.id.moveY(y)
+})
+
+Object.member('show', function(){
+	this.id.show()
+})
+Object.member('hide', function(){
+	this.id.hide()
+})
+Object.member('open', function(){
+	this.id.open()
+})
+Object.member('close', function(){
+	this.id.close()
+})
+Object.member('lock', function(){
+	this.id.lock()
+})
+Object.member('unlock', function(){
+	this.id.unlock()
+})
+Object.member('isOpened', function(){
+	return this.id.isOpened()
+})
+Object.member('isClosed', function(){
+	return this.id.isClosed()
+})
+Object.member('isLocked', function(){
+	return this.id.isLocked()
+})
+Object.member('pick', function(){
+	this.id.pick()
+})
+Object.member('isPicked', function(){
+	return this.id.isPicked()
+})
+
+//////// Door Definition
+
+function Door(room, name, closedImage, openedImage, connectedTo, width, x, y){
+	Object.call(this, room, name, closedImage)
+
+	// Door properties
+	this.closedImage = closedImage
+	this.openedImage = openedImage
+	this.connectedTo = connectedTo
+
+	this.resize(width)
+	this.locate(x, y)
+}
+// inherited from Object
+Door.prototype = new Object()
+
+Door.member('onClick', function(){
+	if (!this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	}
+	else if (this.id.isOpened()){
+		if (this.connectedTo !== undefined){
+			Game.move(this.connectedTo)
+		}
+		else {
+			Game.end()
+		}
+	}
+})
+Door.member('onOpen', function(){
+	this.id.setSprite(this.openedImage)
+})
+Door.member('onClose', function(){
+	this.id.setSprite(this.closedImage)
+})
+
+
+//////// Keypad Definition
+
+function Keypad(room, name, image, password, callback){
+	Object.call(this, room, name, image)
+
+	// Keypad properties
+	this.password = password
+	this.callback = callback
+}
+// inherited from Object
+Keypad.prototype = new Object()
+
+Keypad.member('onClick', function(){
+	showKeypad('number', this.password, this.callback)
+})
+
+
+//////// DoorLock Definition
+function DoorLock(room, name, image, password, door, message){
+	Keypad.call(this, room, name, image, password, function(){
+		printMessage(message)
+		door.unlock()
+	})
+}
+// inherited from Object
+DoorLock.prototype = new Keypad()
+
+/////// Item Definition
+
+function Item(room, name, image, width, x, y){
+	Object.call(this, room, name, image)
+	this.resize(width)
+	this.locate(x, y)
+}
+// inherited from Object
+Item.prototype = new Object()
+
+Item.member('onClick', function(){
+	this.id.pick()
+})
+
+
+Item.member('isHanded', function(){
+	return Game.handItem() == this.id
+})
+
+//이까지 재정의
+
+
+
+
+room1 = new Room('room1', '배경-1.png')		// 변수명과 이름이 일치해야 한다.
+room2 = new Room('room2', '배경-2.png')		// 변수명과 이름이 일치해야 한다.
+room3 = new Room('room3', '배경-3.png')		// 변수명과 이름이 일치해야 한다.
+
+room1.door1 = new Door(room1, 'door1', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room2, 120, 1000, 312)
+//room1.door1.resize(120)
+//room1.door1.locate(1000, 312)
+room1.door1.lock()
+
+
+
+//Item.member('onClick', function(){
+//	this.id.pick()
+//})
+
+Item.member('onClick', function(){
+//	printMessage(this.name)
+})
+
+//방1 배경소품
+room1.table=new Item(room1, 'table', '1큰상.png', 1000, 700, 500)
+room1.aircon=new Item(room1, 'aircon', '1에어컨.png', 300, 150, 100)
+room1.tv=new Item(room1, 'tv', '1TV.png', 200, 300, 200)
+room1.carpet=new Item(room1, 'carpet', '1카페트.png', 500, 600, 600)
+room1.fdoor=new Item(room1, 'fdoor', '1현관문.png', 200, 100, 400)
+room1.fdoor.onClick=function()
+{
+	printMessage("밖에 못 나감")
+}
+
+room1.window=new Item(room1, 'window', '1창문.png', 200, 800, 200) 
+memcounter=0
+room1.mem=new Item(room1, 'mem', '책.png', 50, 650, 500)
+room1.mem.onClick=function()
+{
+	if(memcounter==0)
+	{
+		memcounter++
+		playSound("브롤스타즈 BGM - 메인화면.wav")
+	}
+	room1.memo.show()			
+}
+
+//고정 객체
+
+room1.carpetshawdow=new Item(room1, 'carpetshawdow', '카페트아래.png', 200, 600, 600)
+room1.carpetshawdow.close()
+room1.carpetshawdow.onClick=function(){
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.envelope.isHanded())
+			{
+				this.id.open()
+				printMessage("비상금은 항상 아무도 모르게 챙겨두자…")
+		
+			}
+			else if(room1.milkpowder.isHanded())
+			{
+				//playSound("애기우는소리.wav")
+				//game.setGameoverMessage("애기 울림")
+
+				//game.gameover()
+		
+				printMessage(room1.milkpowder.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.goldfish.isHanded())
+			{
+				//game.setGameoverMessage("졌다! 오늘 저녁은 피쉬앤그릴이다!")
+				//game.gameover()
+		
+				printMessage(room1.goldfish.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.soju.isHanded())
+			{
+
+				//game.setGameoverMessage("애기 : 아빠 이따 밤에 엄마 몰래 한잔 기기?")
+				//game.gameover()
+
+				printMessage(room1.soju.name+"잘못 넣음 게임 종료")
+			}
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+room1.cradle=new Item(room1, 'cradle', '1요람.png', 200, 50, 600)
+room1.cradle.close()
+room1.cradle.onClick=function(){
+	if(this.id.isClosed())//close 상태일 때
+	{
+		if(!Game.handItem())//아무것도 안 가지고 있을 때
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.milkpowder.isHanded())
+			{
+				this.id.open()
+				printMessage("애기 서울대 가라고 서울우유만 사는건 오바아닌가;;")
+		
+			}
+			else if(room1.envelope.isHanded())
+			{
+				//game.setGameoverMessage(msg)
+				//playYoutube("https://www.youtube.com/watch?v=PVcpF7J2oXs")
+				//game.gameover()
+				printMessage(room1.envelope.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.goldfish.isHanded())
+			{
+				//game.setGameoverMessage("졌다! 오늘 저녁은 피쉬앤그릴이다!")
+				//game.gameover()
+				printMessage(room1.goldfish.name+"잘못 넣음 게임 종료")
+				
+		
+			}
+			else if(room1.soju.isHanded())
+			{	
+				//game.setGameoverMessage("애기 : 아빠 이따 밤에 엄마 몰래 한잔 기기?")
+				//game.gameover()
+				printMessage(room1.soju.name+"잘못 넣음 게임 종료")
+			}
+		}
+	}
+	else//open 상태
+	{
+		printMessage("완료")
+	}
+}
+
+
+
+room1.sofa=new Item(room1, 'sofa', '1쇼파.png', 200, 1000, 700)
+room1.sofa.close()
+room1.sofa.onClick=function(){
+
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.soju.isHanded())
+			{
+				this.id.open()
+				printMessage("이따 마눌님 주무시면 혼술 조져야지 히히")
+		
+			}
+			else if(room1.milkpowder.isHanded())
+			{
+				//playSound("애기우는소리.wav")
+				//game.gameover()
+
+				printMessage(room1.milkpowder.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.goldfish.isHanded())
+			{
+				//game.setGameoverMessage("졌다! 오늘 저녁은 피쉬앤그릴이다!")
+				//game.gameover()
+
+				printMessage(room1.goldfish.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.envelope.isHanded())
+			{
+				//game.setGameoverMessage("동작 그만! 밑장 빼기냐!")
+
+				//game.gameover()
+
+				printMessage(room1.envelope.name+"잘못 넣음 게임 종료")
+			}
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+room1.fishbowl=new Item(room1, 'fishbowl', '1어항.png', 200, 300, 600)
+room1.fishbowl.close()
+room1.fishbowl.onClick=function(){
+	if(this.id.isClosed())
+	{
+		if(!Game.handItem())
+		{
+			printMessage(this.name)	
+		}
+		else
+		{
+			if(room1.goldfish.isHanded())
+			{
+				this.id.open()
+				printMessage("goldfish")
+		
+			}
+			else if(room1.milkpowder.isHanded())
+			{
+				//playSound("애기우는소리.wav")
+				//game.setGameoverMessage("애기 울림")
+
+
+				//game.gameover()
+
+				printMessage(room1.milkpowder.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.soju.isHanded())
+			{
+				//game.setGameoverMessage("애기 : 아빠 이따 밤에 엄마 몰래 한잔 기기?")
+				//game.gameover()
+
+				printMessage(room1.soju.name+"잘못 넣음 게임 종료")
+		
+			}
+			else if(room1.envelope.isHanded())
+			{
+				//game.setGameoverMessage("동작 그만! 밑장 빼기냐!")
+
+				//game.gameover()
+
+				printMessage(room1.envelope.name+"잘못 넣음 게임 종료")
+			}
+
+		}
+	}
+	else
+	{
+		printMessage("완료")
+	}
+}
+
+//방1 사용 아이템
+
+goldfishcounter=0
+room1.goldfish=new Item(room1, 'goldfish', '1금붕어.png', 200, 900, 500)
+room1.goldfish.onClick=function(){
+	if(goldfishcounter==0)
+	{
+		printMessage("금붕어 설명")
+		goldfishcounter++
+	}
+	else
+	{
+		room1.goldfish.pick()
+		printMessage("금붕어 획득")
+	}
+
+}
+
+milkpowdercounter=0
+room1.milkpowder=new Item(room1, 'milkpowder', '1우유.png', 200, 300, 500)
+room1.milkpowder.onClick=function(){
+	if(milkpowdercounter==0)
+	{
+		printMessage("분유통 설명")
+		milkpowdercounter++
+	}
+	else
+	{
+		room1.milkpowder.pick()
+		printMessage("분유통 획득")
+	}
+
+}
+
+sojucounter=0
+room1.soju=new Item(room1, 'soju', '1소주병.png', 200, 400, 500)
+room1.soju.onClick=function(){
+	if(sojucounter==0)
+	{
+		printMessage("소주 설명")
+		sojucounter++
+	}
+	else
+	{
+		room1.soju.pick()
+		printMessage("소주 획득")
+	}
+
+}
+
+envelopecounter=0
+room1.envelope=new Item(room1, 'envelope', '1돈.png', 200, 500, 450)
+room1.envelope.onClick=function(){
+	if(envelopecounter==0)
+	{
+		printMessage("돈봉투 설명")
+		envelopecounter++
+	}
+	else
+	{
+		room1.envelope.pick()
+		printMessage("돈봉투 획득")
+	}
+
+}
+
+
+
+
+room1.door1.onClick = function(){
+
+	if(room1.door1.isOpened()){
+		//playSound("애기우는소리.wav")
+		Game.move(room2)
+	}
+	else if(room1.door1.isLocked())
+	{
+		printMessage("잠겨있음")		
+	}	
+	else if(room1.cradle.isOpened()&&room1.fishbowl.isOpened()&&room1.carpetshadow.isOpened()&&room1.sofa.isOpened())
+	{
+		room1.door1.open()	
+	}
+	
+}
+
+room1.memo=new Item(room1, 'memo', '쪽지내용.png', 700, 600, 400)
+room1.memo.hide()
+room1.memo.onClick=function()
+{
+	room1.memo.hide()
+	printMessage("연애할 때에는 분명 이러지 않았었는데…밥차려준게 어디냐. 하 일단 청소부터 해볼까..")	
+}
+
+
+//방 2 시작
+room2.door1 = new Door(room2, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room1, 120, 200, 312)
+room2.door1.open()
+
+
+
+room2.door2 = new Door(room2, 'door2', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room3, 120, 1000, 312)
+room2.door2.lock()
+//배경
+room2.bed=new Item(room2, 'bed', '2아기침대-측면.png',100, 800, 450) 
+room2.mobile=new Item(room2, 'mobile', '2아기모빌.png',100, 800, 400)
+
+//사용아이템
+room2.milk=new Item(room2, 'milk', '2우유.png', 100, 200, 600)
+room2.milk.onClick=function()
+{
+	room2.milk.pick()	
+} 
+room2.diaper=new Item(room2, 'diaper', 't1.png', 100, 1100, 400) 
+room2.diaper.onClick=function()
+{
+	room2.diaper.pick()	
+} 
+room2.toy=new Item(room2, 'toy', 't2.png', 100, 700, 550) 
+room2.toy.onClick=function()
+{
+	room2.toy.pick()	
+}
+
+//아기
+babycounter=0
+bxp=500
+byp=500
+bsize=100
+babyhit=0
+babystate=0
+onmilk=0
+ondiaper=0
+ontoy=0
+tim=15
+function phase(){
+				bxp=Math.floor(Math.random()*1000+100)
+				byp=Math.floor(Math.random()*200+400)
+				room2.baby.locate(bxp, byp)
+				babystate=Math.floor(Math.random()*2)
+				//playSound("애기웃는소리.wav")
+				game.setTime(tim)
+				if(babystate==0)//밥 페이즈
+				{
+					onmilk=1
+					ondiaper=0
+					ontoy=0				
+					room2.baby.setSprite('2아기1_밥.png')				
+				}
+				else if(babystate==1)//기저귀 페이즈
+				{
+					ondiaper=1
+					onmilk=0
+					ontoy=0
+					room2.baby.setSprite('2아기1_기저귀.png')				
+				}
+}
+function toyphase(){
+			ontoy=1
+			ondiaper=0
+			onmilk=0
+			//playSound("애기화난소리.wav")
+			room2.baby.setSprite('2아기2.png')	
+			game.setTime(tim)
+			babycounter++		
+			bxp=Math.floor(Math.random()*1000+100)
+			byp=Math.floor(Math.random()*200+400)		
+			bsize+=70
+			room2.baby.resize(bsize)
+			room2.baby.locate(bxp, byp)
+	
+}
+function sleepbaby()
+{
+if(babyhit==3)
+{
+	room2.baby.close()
+	room2.baby.resize(100)
+	room2.baby.locate(800, 450)
+				
+	room2.baby.setSprite("t3.png")
+	room2.door2.close()
+	game.hideTimer()
+}	
+}	
+room2.baby=new Item(room2, 'baby', '2아기1.png', bsize, bxp, byp) 
+room2.baby.open()
+room2.baby.onClick=function()
+{
+if(room2.baby.isOpened())
+{	
+	if(babycounter==0)//최초 클릭
+	{
+		game.setTimer(tim, 1,"")
+		toyphase()
+	}//if(babycounter==0)//최초 클릭
+	else//두번째부터
+	{
+		if(game.getLeftTime()>2)//시간초 성공
+		{
+			if(onmilk==1)
+			{
+				if(room2.milk.isHanded())
+				{
+					phase()
+					babyhit++
+					sleepbaby()
+				}
+				else
+				{
+					toyphase()
+				}
+				
+			}
+			else if(ondiaper==1)
+			{
+				if(room2.diaper.isHanded())
+				{
+					phase()
+					babyhit++
+					sleepbaby()
+				}
+				else
+				{
+					toyphase()
+				}
+			}
+			else if(ontoy==1)
+			{
+				if(room2.toy.isHanded())
+				{
+					phase()
+					
+				}
+				else
+				{
+					toyphase()
+				}
+			}
+			
+		}
+		else if(babycounter==5)//게임오버
+		{
+			
+			//playSound("애기화난소리.wav")
+			room2.baby.setSprite('2아기2.png')
+			room2.baby.resize(1200)
+			room2.baby.locate(600, 300)
+			game.setGameoverMessage("YOU DIED")
+			game.setTime(1)		
+		}
+		else if(babycounter==6)
+		{
+		}
+		else//조건 실패 딸랑이 페이즈로 변환
+		{
+			toyphase()
+		}
+	}//else//두번째부터
+	printMessage("babystate"+babystate+" "+"onmilk "+onmilk+"onidaper "+ondiaper+"ontoy "+ontoy+"babycounter "+babycounter)
+}
+else
+{
+	printMessage("아기가 잔다")
+}
+}
+
+room2.door2.onClick=function()
+{
+	if(room2.door2.isClosed())
+	{
+		room2.door2.open()
+	}
+	else if(room2.door2.isOpened())
+	{
+		Game.move(room3)
+	}
+	else(room2.door2.isLocked())
+	{
+		printMessage("잠겨있다")
+	}
+}
+
+//방 3 시작
+
+Game.start(room2, '평화로운 주말….어제 회식 때문에 너무 머리가 아프다… 엇 저기 아침밥상이 있군 아이조아')
